3b2c5c4d3e3c03b1feb968b4b8aec591a67219f1
leandrohcn <leandro1111bfr@gmail.com>
2025-06-10 23:03:07 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,31 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("HELLO WORLd");
+        ContaTerminal conta;
+        String agencia;
+        int numero;
+        String nomeCliente;
+        float saldo;
+
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Por favor, digite o numero da conta: ");
+        numero = entrada.nextInt();
+        entrada.nextLine();
+
+        System.out.println("Por favor, digite o numero da agencia: ");
+        agencia = entrada.nextLine();
+
+        System.out.println("Por favor, digite o nome do cliente: ");
+        nomeCliente = entrada.nextLine();
+
+        System.out.println("Por favor, digite o saldo: ");
+        saldo = entrada.nextFloat();
+
+        conta = new ContaTerminal(numero, agencia, nomeCliente, saldo);
+        System.out.println(conta);
+
+        entrada.close();
         }
     }
